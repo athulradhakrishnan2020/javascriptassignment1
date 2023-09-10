035430a5ea12edd4cddb689733d084efbd4f8220
athulkrishnan <90600183+athulradhakrishnan2020@users.noreply.github.com>
2023-09-10 09:54:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// String
+var myString = "Hello, world!";
+
+// Number
+var myNumber = 42;
+
+// Float (Floating-point number)
+var myFloat = 3.14;
+
+// Boolean
+var myBoolean = true;
+
+// Logging the variables
+console.log("String:", myString);
+console.log("Number:", myNumber);
+console.log("Float:", myFloat);
+console.log("Boolean:", myBoolean);
